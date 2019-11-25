29deebf12a09e688c0f41d51df8ee390aab0eef6
Jason <1358681631@qq.com>
2019-11-25 22:42:27 +0800

    mfp_20191125a:mfp_model_roiprebox_tinyinput_rpn_premaskpart.py使用mask+part作为输入

--- train_mfp.py
+++ train_mfp.py
@@ -15,11 +15,11 @@ config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
 from configs.vipdataset_for_mfp import VIPDatasetForMFP
-from models.mfp_model_roiprebox_tinyinput_rpn_preimagemask import MFP, MFPConfig
+from models.mfp_model_roiprebox_tinyinput_rpn_premaskpart import MFP, MFPConfig
 
 
 class trainConfig(MFPConfig):
-    NAME = "mfp_20191124a"
+    NAME = "mfp_20191125a"
     # NAME = "mfp_debug"
     GPU_COUNT = 1
     IMAGES_PER_GPU = 6
