6aca465809d19ced7babf8f6349f0e124037bca9
Jason <1358681631@qq.com>
2019-11-29 15:34:21 +0800

    hpa_20191129b:hpa_resfpn_c5d_edgam_e357_part357_partse, resfpn模块+resnet第五阶段空洞卷积率为2,3,5+encoder和decoder添加gam+encoder的空洞率改为3,5,7+part分支的空洞率改为3,5,7+part分支se

--- train_hpa.py
+++ train_hpa.py
@@ -9,17 +9,17 @@ MACHINE_NAME = platform.node()
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 sys.path.insert(0, os.getcwd())
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
 from configs.vipdataset_for_mfp import VIPDatasetForMFP
-from models.hpa_resfpn_c5d_edgam_e357_part357 import HPANet, HPAConfig
+from models.hpa_resfpn_c5d_edgam_e357_part357_partse import HPANet, HPAConfig
 
 
 class trainConfig(HPAConfig):
-    NAME = "hpa_20191129a"
+    NAME = "hpa_20191129b"
     # NAME = "hpa_debug"
     GPU_COUNT = 1
     IMAGES_PER_GPU = 4
