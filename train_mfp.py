82c3f70cd83c93d57ee44a7b19a1e94c9d5aac0b
Jason <1358681631@qq.com>
2019-12-04 00:45:19 +0800

    mfp_20191204b:mfp_resfpn_c5d_edgamf256_e357_part357_partse_image_3d3igru.py, resfpn模块+resnet第五阶段空洞卷积率为2,3,5+encoder和decoder添加gam256+encoder的空洞率改为3,5,7+part分支的空洞率改为3,5,7+part分支se+3d3igru+image

--- train_mfp.py
+++ train_mfp.py
@@ -9,17 +9,17 @@ MACHINE_NAME = platform.node()
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 sys.path.insert(0, os.getcwd())
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
 from configs.vipdataset_for_mfp import VIPDatasetForMFP
-from models.mfp_resfpn_c5d_edgamf256_e357_part357_partse_image_3d2 import MFPNet, MFPConfig
+from models.mfp_resfpn_c5d_edgamf256_e357_part357_partse_image_3d3igru import MFPNet, MFPConfig
 
 
 class trainConfig(MFPConfig):
-    NAME = "mfp_20191204a"
+    NAME = "mfp_20191204b"
     # NAME = "mfp_debug"
     GPU_COUNT = 1
     IMAGES_PER_GPU = 4
